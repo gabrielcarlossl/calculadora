e142f110793651004cbdbf8b1e612f0d2fd0cbc2
Gabriel Carlos <gabrielcarlossleite@gmail.com>
2022-06-27 12:18:33 -0300

    Envio final do projeto

---
+++ src/components/Button.jsx
@@ -0,0 +1,16 @@
+/* eslint-disable import/no-anonymous-default-export */
+import React from "react";
+import './Button.css'
+
+export default props => 
+    <button className={`
+        button
+        ${props.operation ? 'operation' : ''}
+        ${props.double ? 'double' : ''}
+        ${props.triple ? 'triple' : ''}
+    `}
+    onClick={e =>props.click(props.label)}
+    >
+
+        {props.label}
+    </button>
